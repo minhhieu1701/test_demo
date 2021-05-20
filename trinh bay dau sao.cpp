4d0b85a60713b122c7774e6c2f4b4733259a954b
minhhieu1701 <ngminhhieu1701@gmail.com>
2021-05-20 10:13:38 +0200

    Them demo

---
+++ trinh bay dau sao.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+int main ()
+{
+	int i,j,k;
+	printf ("nhap so nguyen : ");
+	scanf ("%d", &k);
+	printf ("\n");
+	for (i=1; i<=k ; i++)
+	{
+		printf("\n");
+		for (j=1; j<=k; j++)
+		printf ("*");
+		}
+}
+	
